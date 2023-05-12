002ebf9883c74d5796644f76b8adebcc882fe327
Abhishek Alimchandani <113540283+Abhishek-1624@users.noreply.github.com>
2023-05-12 13:11:27 +0530

    Initial commit

---
+++ src/components/Note.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+function Note() {
+  return (
+    <div className="note">
+      <h1>This is note tittle</h1>
+      <p>This is tittle content</p>
+    </div>
+  );
+}
+
+export default Note;
